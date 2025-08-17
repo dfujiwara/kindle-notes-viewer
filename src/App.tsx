98d56c2408675dc249bfc99a5a7bf29dd8aefe72
Daisuke Fujiwara <daisuke.fujiwara@gmail.com>
2025-08-16 21:17:22 -0700

    Add header and footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,7 @@
 import { useState } from "react";
 import { ErrorBoundary } from "react-error-boundary";
+import { Footer } from "./components/Footer";
+import { Header } from "./components/Header";
 import { ErrorFallback } from "./ErrorFallback";
 import "./App.css";
 
@@ -8,21 +10,27 @@ function App() {
 
   return (
     <ErrorBoundary FallbackComponent={ErrorFallback}>
-      <div>
-        <h1 className="text-red-200">Kindle Notes Frontend</h1>
-        <div className="card">
-          <button type="button" onClick={() => setCount((count) => count + 1)}>
-            count is {count}
-          </button>
-          <p>
-            Edit <code>src/App.tsx</code> and save to test HMR
+      <div className="min-h-screen flex flex-col">
+        <Header />
+        <main className="flex-1 flex flex-col justify-center items-center p-8">
+          <div className="p-8">
+            <button
+              type="button"
+              onClick={() => setCount((count) => count + 1)}
+            >
+              count is {count}
+            </button>
+            <p>
+              Edit <code>src/App.tsx</code> and save to test HMR
+            </p>
+            <p>The mode is {import.meta.env.MODE}</p>
+            <p>The API URL is {import.meta.env.VITE_API_URL}</p>
+          </div>
+          <p className="text-zinc-500">
+            Click on the Vite and React logos to learn more
           </p>
-          <p>The mode is {import.meta.env.MODE}</p>
-          <p>The API URL is {import.meta.env.VITE_API_URL}</p>
-        </div>
-        <p className="read-the-docs">
-          Click on the Vite and React logos to learn more
-        </p>
+        </main>
+        <Footer />
       </div>
     </ErrorBoundary>
   );
