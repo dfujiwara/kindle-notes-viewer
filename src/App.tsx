d438ab51bee113723fb114bbbfa69e648b4da01d
Daisuke Fujiwara <daisuke.fujiwara@gmail.com>
2025-08-30 19:49:26 -0700

    Move the Suspense component further down the component tree (#27)
    Will probably have more than one suspense component in the future.

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,27 @@
+import { Suspense } from "react";
 import { ErrorBoundary } from "react-error-boundary";
 import { Route, Routes } from "react-router";
 import { Footer, Header } from "src/components";
 import { ErrorFallback } from "src/components/ErrorFallback";
+import { LoadingIndicator } from "src/components/LoadingIndicator.tsx";
 import { HomePage } from "src/pages/HomePage";
 import "./App.css";
 
 function App() {
   return (
     <ErrorBoundary FallbackComponent={ErrorFallback}>
-      <div className="min-h-screen flex flex-col">
-        <Header />
-        <main className="flex-1 p-8">
-          <Routes>
-            <Route path="/" element={<HomePage />} />
-            <Route path="/books" element={<h1>books</h1>} />
-          </Routes>
-        </main>
-        <Footer />
-      </div>
+      <Suspense fallback={<LoadingIndicator />}>
+        <div className="min-h-screen flex flex-col">
+          <Header />
+          <main className="flex-1 p-8">
+            <Routes>
+              <Route path="/" element={<HomePage />} />
+              <Route path="/books" element={<h1>books</h1>} />
+            </Routes>
+          </main>
+          <Footer />
+        </div>
+      </Suspense>
     </ErrorBoundary>
   );
 }
