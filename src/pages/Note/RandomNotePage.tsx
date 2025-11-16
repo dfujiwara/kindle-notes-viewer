5928c990c42c64d978445f359205e1fbbb1a11e6
Daisuke Fujiwara <daisuke.fujiwara@gmail.com>
2025-11-16 13:41:58 -0800

    Remove suspence from the random page
    since it seems like it's not triggering appropriately with the
    combination of react router. So I have decided to handle the loading
    indicator in my own code.

--- src/pages/Note/RandomNotePage.tsx
+++ src/pages/Note/RandomNotePage.tsx
@@ -1,9 +1,12 @@
 import { useNavigate } from "react-router";
-import { notesService, useApiSuspenseQuery } from "src/api";
+import { notesService, useApiQuery } from "src/api";
+import { LoadingIndicator } from "src/components/LoadingIndicator";
 import { NoteDescription } from "./NoteDescription";
 
 export function RandomNotePage() {
-  const result = useApiSuspenseQuery(
+  const navigate = useNavigate();
+
+  const { data, isLoading, isError, error } = useApiQuery(
     ["notes", "random"],
     () => notesService.getRandomNote(),
     {
@@ -11,12 +14,25 @@ export function RandomNotePage() {
       gcTime: 0,
     },
   );
-  const navigate = useNavigate();
-  const { book } = result.data;
+
+  if (isLoading) {
+    return <LoadingIndicator />;
+  }
+
+  if (isError) {
+    throw error;
+  }
+
+  if (!data) {
+    return <LoadingIndicator />;
+  }
+
+  const { book } = data;
+
   return (
     <div>
       <NoteDescription
-        detailedNote={result.data}
+        detailedNote={data}
         onBookClick={() => {
           navigate(`/books/${book.id}/`);
         }}
