356c2bd6db6e5945dbd320d17d38390619f3f071
Daisuke Fujiwara <daisuke.fujiwara@gmail.com>
2025-11-30 15:19:10 -0800

    remove NotePage since it's no longer used (#99)

--- src/pages/Note/NotePage.tsx
+++ src/pages/Note/NotePage.tsx
@@ -1,35 +1,7 @@
 import { useNavigate, useParams } from "react-router";
-import { notesService, useApiSuspenseQuery } from "src/api";
 import { LoadingIndicator } from "src/components/LoadingIndicator";
 import { NoteDescription } from "./NoteDescription";
 import { useStreamedDetailedNote } from "./useStreamedDetailedNote";
-
-export function NotePage() {
-  const { bookId, noteId } = useParams<{ bookId: string; noteId: string }>();
-  if (bookId === undefined || noteId === undefined) {
-    throw new Error("Book ID or Note ID is not defined in the URL");
-  }
-  const result = useApiSuspenseQuery(
-    ["note", bookId, noteId],
-    () => notesService.getNote(bookId, noteId),
-    { refetchOnWindowFocus: false },
-  );
-  const navigate = useNavigate();
-  return (
-    <div>
-      <NoteDescription
-        book={result.data.book}
-        note={result.data.note}
-        additionalContext={result.data.additionalContext}
-        relatedNotes={result.data.relatedNotes}
-        onBookClick={() => navigate(`/books/${bookId}`)}
-        onRelatedNoteClick={(relatedNoteId) => {
-          navigate(`/books/${bookId}/notes/${relatedNoteId}`);
-        }}
-      />
-    </div>
-  );
-}
 
 export function StreamedNotePage() {
   const navigate = useNavigate();
--- src/pages/index.ts
+++ src/pages/index.ts
@@ -1,6 +1,6 @@
 export { BookPage } from "./Book/BookPage";
 export { HomePage } from "./Home/HomePage";
-export { NotePage, StreamedNotePage } from "./Note/NotePage";
+export { StreamedNotePage } from "./Note/NotePage";
 export { RandomNotePage } from "./Note/RandomNotePage";
 export { SearchPage } from "./Search/SearchPage";
 export { UploadPage } from "./Upload/UploadPage";
