d552857194bfcbfedeb000608da6776e1feede69
Daisuke Fujiwara <daisuke.fujiwara@gmail.com>
2025-11-30 15:24:37 -0800

    remove unused NotesService methods
    since those API endpoints aren't necessary anymore due to using
    the streaming capability.

--- src/api/notesService.ts
+++ src/api/notesService.ts
@@ -21,13 +21,6 @@ interface KindleInitialNoteApiResponse {
   related_notes: KindleNoteApiResponse[];
 }
 
-interface KindleDetailedNoteApiResponse {
-  book: KindleBook;
-  note: KindleNoteApiResponse;
-  additional_context: string;
-  related_notes: KindleNoteApiResponse[];
-}
-
 interface KindleNoteBundleApiResponse {
   book: KindleBook;
   notes: KindleNoteApiResponse[];
@@ -56,19 +49,8 @@ const mapInitialDetailedNote = (
   relatedNotes: apiNote.related_notes.map(mapNote),
 });
 
-const mapDetailedNote = (
-  apiNote: KindleDetailedNoteApiResponse,
-): KindleDetailedNote => ({
-  book: apiNote.book,
-  note: mapNote(apiNote.note),
-  additionalContext: apiNote.additional_context,
-  relatedNotes: apiNote.related_notes.map(mapNote),
-});
-
 const ENDPOINTS = {
   LIST: (bookId: string) => `/books/${bookId}/notes`,
-  NOTE: (bookId: string, noteId: string) => `/books/${bookId}/notes/${noteId}`,
-  RANDOM: "/random",
   STREAM_RANDOM: "/random/stream",
   STREAM_NOTE: (bookId: string, noteId: string) =>
     `/books/${bookId}/notes/${noteId}/stream`,
@@ -99,29 +81,6 @@ export class NotesService {
     return {
       ...response,
       data: mapNoteBundle(response.data),
-    };
-  }
-
-  async getNote(
-    bookId: string,
-    noteId: string,
-  ): Promise<ApiResponse<KindleDetailedNote>> {
-    const response = await httpClient.request<KindleDetailedNoteApiResponse>(
-      ENDPOINTS.NOTE(bookId, noteId),
-    );
-    return {
-      ...response,
-      data: mapDetailedNote(response.data),
-    };
-  }
-
-  async getRandomNote(): Promise<ApiResponse<KindleDetailedNote>> {
-    const response = await httpClient.request<KindleDetailedNoteApiResponse>(
-      ENDPOINTS.RANDOM,
-    );
-    return {
-      ...response,
-      data: mapDetailedNote(response.data),
     };
   }
 
