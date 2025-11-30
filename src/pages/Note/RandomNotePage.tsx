dcccca18636fb02cd6f7dc2eac804a55f0ddf4ba
Daisuke Fujiwara <daisuke.fujiwara@gmail.com>
2025-11-30 08:25:24 -0800

    Remove unused RandomNotePage component since we are now streaming (#95)
    with StreamedRandomNotePage

--- src/pages/Note/RandomNotePage.tsx
+++ src/pages/Note/RandomNotePage.tsx
@@ -1,52 +1,7 @@
 import { useNavigate } from "react-router";
-import { notesService, useApiQuery } from "src/api";
 import { LoadingIndicator } from "src/components/LoadingIndicator";
 import { NoteDescription } from "./NoteDescription";
 import { useStreamedDetailedNote } from "./useStreamedDetailedNote";
-
-export function RandomNotePage() {
-  const navigate = useNavigate();
-
-  const { data, isLoading, isError, error } = useApiQuery(
-    ["notes", "random"],
-    () => notesService.getRandomNote(),
-    {
-      refetchOnWindowFocus: false,
-      gcTime: 0,
-    },
-  );
-
-  if (isLoading) {
-    return <LoadingIndicator />;
-  }
-
-  if (isError) {
-    throw error;
-  }
-
-  if (!data) {
-    return <LoadingIndicator />;
-  }
-
-  const { book, note, additionalContext, relatedNotes } = data;
-
-  return (
-    <div>
-      <NoteDescription
-        book={book}
-        note={note}
-        additionalContext={additionalContext}
-        relatedNotes={relatedNotes}
-        onBookClick={() => {
-          navigate(`/books/${book.id}/`);
-        }}
-        onRelatedNoteClick={(relatedNoteId) => {
-          navigate(`/books/${book.id}/notes/${relatedNoteId}`);
-        }}
-      />
-    </div>
-  );
-}
 
 export function StreamedRandomNotePage() {
   const navigate = useNavigate();
